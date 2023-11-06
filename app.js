d22fd6a8b21c1602d3f05916b4634178f5930d62
KELASU <95676450+KELASU@users.noreply.github.com>
2023-11-07 05:34:16 +0700

    e2e2e

---
+++ app.js
@@ -0,0 +1,82 @@
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+var x=document.getElementById('login');
+var y=document.getElementById('register');
+var z=document.getElementById('btn');
+function register()
+{
+  x.style.left='-400px';
+  y.style.left='50px';
+  z.style.left='110px';
+}
+function login()
+{
+  x.style.left='50px';
+  y.style.left='450px';
+  z.style.left='0px';
+}
+
+
+var closeFormBtn = document.querySelector('.form-box .close-btn');
+closeFormBtn.onclick = function() {
+  modal.style.display = "none";
+};
+
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        modal.style.display = "none";
+    }
+}
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btns");
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+  // Clone the wrapper
+  dark = !dark;
+  let clone = big_wrapper.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
+
+  document.body.classList.add("stop-scrolling");
+
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
+}
+
+events();
